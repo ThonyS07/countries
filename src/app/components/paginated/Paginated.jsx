b64df44fb9070985fba80cbaa13c9841f37394f1
Thony Silva <thonysilva1993@gmail.com>
2024-06-22 19:21:24 -0500

    componente Paginated

--- src/app/components/paginated/Paginated.jsx
+++ src/app/components/paginated/Paginated.jsx
@@ -1,8 +1,5 @@
 //? React
 import React from "react";
-
-//? Styles
-import styles from "./Paginated.module.css";
 import rightArrow from "../../assets/img/flechaderecha.png";
 import rightArrowDisabled from "../../assets/img/flechaderechaoff.png";
 import leftArrow from "../../assets/img/flechaizquierda.png";
@@ -26,10 +23,14 @@ const Paginated = ({ currentPage, maxPage }) => {
 	};
 
 	return (
-		<div className={styles.paginated}>
+		<div className='flex justify-center items-center'>
 			<button
 				disabled={currentPage <= 1}
-				className={currentPage <= 1 ? styles.disabled : styles.pageButton}
+				className={
+					currentPage <= 1
+						? "flex w-6 h-6 border-black bg-none overflow-hidden right-[5px] cursor-not-allowed"
+						: "flex w-6 h-6 border-none bg-none overflow-hidden right-[5px] cursor-pointer hover:bg-white hover:text-black hover:border-black transform hover:scale-[1.1]"
+				}
 				onClick={prevPage}>
 				<Image
 					src={currentPage <= 1 ? leftArrowDisabled : leftArrow}
@@ -41,7 +42,11 @@ const Paginated = ({ currentPage, maxPage }) => {
 			</h4>
 			<button
 				disabled={currentPage >= maxPage}
-				className={currentPage >= maxPage ? styles.disabled : styles.pageButton}
+				className={
+					currentPage >= maxPage
+						? "flex w-6 h-6 border-black bg-none overflow-hidden right-[5px] cursor-not-allowed"
+						: "flex w-6 h-6 border-none bg-none overflow-hidden right-[5px] cursor-pointer hover:bg-white hover:text-black hover:border-black transform hover:scale-[1.1]"
+				}
 				onClick={nextPage}>
 				<Image
 					src={currentPage >= maxPage ? rightArrowDisabled : rightArrow}
